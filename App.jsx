1c86adb98c2970affbd2505658decffb7f109556
unknown <harshmohansahani@gmail.com>
2026-09-12 23:30:13 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App(){
+    return(
+        <>
+        <section className="center">
+            <div className="first-elm">
+                <h1>hello this is a div's heading.</h1>
+            </div>
+        </section>
+        </>
+    )
+
+}
+ export default App;
